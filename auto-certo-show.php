4d48a8eb67cdab2ca1fe412848c3e0ffe849ed17
Douglas Everbero <dougas@3xweb.site>
2024-08-22 13:26:19 -0300

    chore: release v1.4.2

--- auto-certo-show.php
+++ auto-certo-show.php
@@ -2,7 +2,7 @@
 /*
     Plugin Name: Auto Certo Show
     Description: Exibe os veículos da Auto Certo no seu site wordpress.
-    Version: 1.4.0
+    Version: 1.4.2
     Author: Douglas Everbero
 */
 
